58760cc6f3163486b36e6e50dd15aa1d5d029992
Inês Martins <94248907+ines-om@users.noreply.github.com>
2022-04-23 19:15:35 +0100

    Add files via upload
    Fahrenheit to Celsius Converter

---
+++ FtoC.py
@@ -0,0 +1,8 @@
+print("-------------Fahrenheit to Celsius Calculator--------------")
+100
+F = int(input("Insert temperature in Fahrenheit here:  "))
+C = 5 * ((F-32) / 9)
+
+print("\n ")
+print("TEMPERATURE:")
+print("Temperature in Celsius is " + str(int(C)) + "°C ")
